8bdb979a101e90a9c8bff1fffd9e95b2837146f5
Pierre-Nicolas <45076754+PaixHaine@users.noreply.github.com>
2020-04-30 16:29:08 +0200

    Ajout du container search bar

--- CoinCoins_Insolite/src/app/feeds/feeds.module.ts
+++ CoinCoins_Insolite/src/app/feeds/feeds.module.ts
@@ -4,12 +4,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FeedsPage } from './feeds.page';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faMapMarkedAlt, faMapMarkerAlt);
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    FontAwesomeModule,
     RouterModule.forChild([{ path: '', component: FeedsPage }])
   ],
   declarations: [FeedsPage]
