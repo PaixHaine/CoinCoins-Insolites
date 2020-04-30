06107d990a713959374db26da21c205e266a7976
Pierre-Nicolas <45076754+PaixHaine@users.noreply.github.com>
2020-04-30 09:56:34 +0200

    Ajout du template insta

--- CoinCoins_Insolite/src/app/app-routing.module.ts
+++ CoinCoins_Insolite/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'map', loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)}
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
