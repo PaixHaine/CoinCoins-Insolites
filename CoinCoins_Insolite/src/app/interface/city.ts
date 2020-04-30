d3e70f3c61a905faecf71f5e9c81699ca1a2229c
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2020-04-30 13:19:14 +0200

    :sparkles: creation of CityService and interface

---
+++ CoinCoins_Insolite/src/app/interface/city.ts
@@ -0,0 +1,25 @@
+export interface City {
+    "type": string,
+    "geometry": {
+        "type": string,
+        "coordinates": [
+            any,
+            any
+        ]
+    },
+    "properties": {
+        "label": string,
+        "score": any,
+        "id": string,
+        "type": string,
+        "name": string,
+        "postcode": string,
+        "citycode": string,
+        "x": any,
+        "y": any,
+        "population": any,
+        "city": string,
+        "context": string,
+        "importance": any
+    }
+}
---
+++ CoinCoins_Insolite/src/app/services/city.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { FileCity } from '../interface/filecity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUrl(city_name: string,): string
+    {
+        let apiUrl = environment.city.baseUrl;
+        let querySeparator = (apiUrl.indexOf('?') != -1)
+        ? '&'
+        : '?';
+
+        return (apiUrl + querySeparator + `q=${city_name}&type=municipality`);
+    }
+
+  public getConfig(city_name): Observable<FileCity> {
+    var configUrl = this.getUrl(city_name);
+    return this.http.get<FileCity>(configUrl);
+  }
+
+}
