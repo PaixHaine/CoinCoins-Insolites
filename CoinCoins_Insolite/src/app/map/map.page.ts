6e82e5ce9eca126e0b91d7530652818a973be927
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2020-04-30 15:41:51 +0200

    :sparkles: implement map service in map page

--- CoinCoins_Insolite/src/app/map/map.page.ts
+++ CoinCoins_Insolite/src/app/map/map.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Map, tileLayer, marker, icon } from 'leaflet';
-import { Http } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { MapService } from '../services/map.service';
+import { City } from '../interface/city';
 
 
 @Component({
@@ -12,41 +10,22 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  data: City;
 
-  constructor(public http: Http,
-    public plt: Platform,
-    public router: Router) {}
+  constructor(private router: Router, private map: MapService) {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data = this.router.getCurrentNavigation().extras.state.city;
+        console.log(this.data);
+      }
+    }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    this.plt.ready().then(() => {
-      this.http.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev')
-      .pipe(map(res => res.json()))
-      .subscribe(restaurants => this.initMap(restaurants));
-    });
-  }
-
-  initMap(restaurants) {
-    const map = new Map('map').setView([33.6396965, -84.4304574], 23);
-
-    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    const customMarkerIcon = icon({
-      iconUrl: 'assets/icon/favicon.png',
-      iconSize: [64, 64],
-      popupAnchor: [0, -20]
-    });
-
-    restaurants.forEach((restaurant) => {
-      marker([restaurant.position.lat, restaurant.position.lgn], {icon: customMarkerIcon})
-      .bindPopup(`<b>${restaurant.title}</b>`, { autoClose: false })
-      .on('click', () => this.router.navigateByUrl('/restaurant'))
-      .addTo(map).openPopup();
-    });
+    let lat = this.data.geometry.coordinates[0]
+    let long = this.data.geometry.coordinates[1]
+    this.map.getPlaces(lat, long);
   }
 
 }
